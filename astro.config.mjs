6e5b4b4bb61e65a1cef97f7d88c7307f97dd2d72
Kamil2501 <kamilbibwiz@gmail.com>
2024-03-29 16:34:17 +0100

    Added netlify integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import db from "@astrojs/db";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), db()],
-  output: "server"
+  output: "server",
+  adapter: netlify()
 });
