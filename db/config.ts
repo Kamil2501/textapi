cbcb6c7fb3b19a8242f92b8edc68172a4ead6e77
Kamil2501 <kamilbibwiz@gmail.com>
2024-03-29 15:32:54 +0100

    added basic config to db

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,13 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from 'astro:db';
+
+const Post = defineTable({
+  columns: {
+    endpoint: column.text(),
+    content: column.text(),
+  }
+})
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: { Post }
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,8 @@
-import { db } from 'astro:db';
+import { db, Post } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+	await db.insert(Post).values([
+		{ endpoint: "test", content: "testtesttest" }
+	])
 }
